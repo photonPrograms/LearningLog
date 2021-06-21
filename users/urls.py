ebd7c63b313b87ee5240c044826f529e612cbcf1
photonPrograms <yashguptanotes@gmail.com>
2021-06-21 16:48:51 +0530

    First commit

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+"""defines url patters for users"""
+
+from django.urls import path, include
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # include default auth urls
+    path('', include('django.contrib.auth.urls')),
+    # registration pgae
+    path('register/', views.register, name = 'register'),
+]
